0abc73a28f36f50ba40d74f7d098252ab4134474
wangkodok <wangkodok@gmail.com>
2024-03-28 18:33:07 +0900

    데이터를 불러오기 전에 로딩 스피너 반영하기

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,14 @@
 import MainNavigation from "../components/MainNavigation";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 export default function Root() {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </>
